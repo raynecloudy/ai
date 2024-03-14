881391f073fe1e985f0e99b94b4451f44a82e784
qwertyy <129226914+qwertyy-dev@users.noreply.github.com>
2024-03-13 19:50:39 -0700

    icon

--- main.js
+++ main.js
@@ -75,6 +75,7 @@ document.head.innerHTML = `
 <title>artificial idiot</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="logo.png">
 <style>
 html {
 	--active: #96bbff;
